7346ee061714a674ede9f0d3c29c66a7b61a5e96
Ben Niccum <bniccum@apexclearing.com>
2020-11-19 09:48:48 -0800

    Adding Outline and generating a term set and card in the db

--- app.rb
+++ app.rb
@@ -13,6 +13,13 @@ unless dao.database_exists
   db_creator = DatabaseCreator.new
   db_creator.create_database
   print " Done!\n"
+
+  print "Creating a sample terms and card for testing..."
+  terms = TermSet.make_numeric_terms
+  dao.save_term_set(terms)
+  card = terms.generate_random_card
+  dao.save_card(card)
+  print " Done!\n"
 end
 
 # Setup haml templates to use html5 by default
@@ -22,8 +29,37 @@ set :public_folder, 'public'
 # We'll make a CLI log entry real quick so we can see the app running in Docker
 puts 'Running Hello Carrot Cat!'
 
-
+# Landing
+# Show a randomly generated number card
 get '/' do
   @card = TermSet.make_numeric_terms.generate_random_card
   haml :index
+end
+
+# Show an existing Card
+get '/card/:card_id' do
+  "WIP"
+end
+
+# Generate a card from the terms id
+# Should generate/save a card and then redirect to /card/:card_id
+get '/generate/:terms_id' do
+  "WIP"
+end
+
+# Page to define terms to use for cards
+# form posts to /terms
+get '/create' do
+  "WIP"
+end
+
+# Load the /create view with an existing set of terms for viewing or submitting changes for
+# currently this will generate a new terms entry, as we're not supporting edits
+get '/terms/:terms_id' do
+  "WIP"
+end
+
+# Submit terms to be verified/saved
+post '/terms' do
+  "WIP"
 end
